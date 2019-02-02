f51157fd3899f77c768222e87b3cdba7602132d2
cyw <weems1995@hotmail.com>
2019-02-02 10:47:13 +0800

    format test_calculator.py

--- test_caculator.py
+++ test_caculator.py
@@ -1,12 +1,14 @@
 """
 Unit tests for caculator
 """
+
+
 import caculator
 
 class TestCalculator:
 
     def test_addition(self):
-        assert 4 == caculator.add(2,2)
-    
+        assert 4 == caculator.add(2, 2)
+
     def test_subtraction(self):
-        assert 2 == caculator.sub(4,2)
+        assert 2 == caculator.sub(4, 2)
