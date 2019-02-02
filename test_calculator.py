718671dd55823bad488d33d47138751bf8e8a365
cyw <weems1995@hotmail.com>
2019-02-02 11:20:25 +0800

    fix file naming
    caculator -> calculator

--- test_calculator.py
+++ test_calculator.py
@@ -3,13 +3,13 @@ Unit tests for caculator
 """
 
 
-import caculator
+import calculator
 
 
 class TestCalculator:
 
     def test_addition(self):
-        assert 4 == caculator.add(2, 2)
+        assert 4 == calculator.add(2, 2)
 
     def test_subtraction(self):
-        assert 2 == caculator.sub(4, 2)
+        assert 2 == calculator.sub(4, 2)
