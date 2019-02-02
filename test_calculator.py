b6e3fcd4721d625c1794dc5f17292d9be2128050
cyw <weems1995@hotmail.com>
2019-02-02 11:48:41 +0800

    add fail code

--- test_calculator.py
+++ test_calculator.py
@@ -13,3 +13,6 @@ class TestCalculator:
 
     def test_subtraction(self):
         assert 2 == calculator.sub(4, 2)
+
+    def test_multiplication(self):
+        assert 100 == calculator.multiply(2,50)
